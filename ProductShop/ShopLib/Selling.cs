6f48f909c7f0ad152290395c39d15183a61029dc
Artem-Cherkasov <79924549+Artem-Cherkasov@users.noreply.github.com>
2021-04-08 19:10:29 +0300

    Продажа продуктов
    Метод Offer был разделён на несколько методов

--- ProductShop/ShopLib/Selling.cs
+++ ProductShop/ShopLib/Selling.cs
@@ -9,59 +9,66 @@ namespace ShopLib
     public class Selling
     {
         public double product_num;
-        public double sum;
-        public string answer;
+        public string product_name;
+        public int cost;
+        public double sum;        
         public static int count2;
         public string[] list = new string[100];
         public bool selled;
         public Selling() 
         {
-            answer = Convert.ToString(Console.ReadLine());
             sum = 0;
+            cost = 0;
             count2 = 0;
             selled = false;
             product_num = 0;
         }
         public void Offer() 
-        {     
-            if (answer == "yes") 
-            {                
-                selled = true;
-                do
-                {
-                    Console.WriteLine("Ok, choose the number of product. (1, 2, 3 or 4)");
-                    product_num = Convert.ToDouble(Console.ReadLine());
-                    if (product_num == 1)
-                    {
-                        sum = sum + 20;
-                        Console.WriteLine("You bought bread.");
-                        list[count2] = "Bread";
-                    }
-                    else if (product_num == 2)
-                    {
-                        sum = sum + 15;
-                        Console.WriteLine("You bought apple.");
-                        list[count2] = "Apple";
-                    }
-                    else if (product_num == 3)
-                    {
-                        sum = sum + 30;
-                        Console.WriteLine("You bought tomato.");
-                        list[count2] = "Tomato";
-                    }
-                    else if (product_num == 4)
-                    {
-                        sum = sum + 45;
-                        Console.WriteLine("You bought meat.");
-                        list[count2] = "Meat";
-                    }
-                    else
-                        Console.WriteLine("There is no such product like: " + product_num);
-                    Console.WriteLine("Do you want to continue? (yes/no)");
-                    answer = Convert.ToString(Console.ReadLine());
-                    count2++;              
-                } while (answer == "yes");
+        {
+            selled = true;
+            Console.WriteLine("Ok, choose the number of product. (1, 2, 3 or 4)");
+            product_num = Convert.ToDouble(Console.ReadLine());
+            if (product_num == 1)
+            {
+                cost = 20;
+                product_name = "Bread";
             }
+            else if (product_num == 2)
+            {
+                cost = 15;
+                product_name = "Apple";
+            }
+            else if (product_num == 3)
+            {
+                cost = 30;
+                product_name = "Tomato";
+            }
+            else if (product_num == 4)
+            {
+                cost = 45;
+                product_name = "Meat";
+            }
+            else 
+            {
+                Console.WriteLine("There is no such product like: " + product_num);
+                selled = false;
+            }
+        }
+        public void Addition() 
+        {
+            if (selled == true)
+                sum += cost;
+        }
+        public void Insert() 
+        {
+            if (selled == true)
+            {
+                Console.WriteLine("You bought " + product_name);
+                list[count2] = product_name;
+                count2++;
+            }
+            else
+                selled = true;
         }
         public void Conclude() 
         {
@@ -70,7 +77,7 @@ namespace ShopLib
                 Console.WriteLine("Thank you for your purchase, here is your cheque:");
                 for (int i = 0; i < count2; i++)
                 {
-                    Console.WriteLine(list[i]);
+                    Console.WriteLine(list[i] + ";");
                 }
                 Console.WriteLine("The total amount of all purchases is equal to: " + sum);
                 Console.WriteLine("Have a nice day!");

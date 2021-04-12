ebe1a95c805b1a52c9f13566d66d9a61dcb76c58
Artem-Cherkasov <79924549+Artem-Cherkasov@users.noreply.github.com>
2021-04-12 21:36:45 +0300

    Класс статистики покупателей
    Содержит переменные о покупателях

---
+++ ShopDataBase/BlogApp/Statistics.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogApp
+{
+    class Statistics
+    {
+        public string Name;
+        public int NumberOfOrders;
+        public int SumOfOrders;
+    }
+}

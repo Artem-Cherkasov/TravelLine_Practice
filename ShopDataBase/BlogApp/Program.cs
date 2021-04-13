9b48dd750c12b1a1dfa6a84c579cd6736c7b5d67
Artem-Cherkasov <79924549+Artem-Cherkasov@users.noreply.github.com>
2021-04-13 22:06:17 +0300

    База данных магазина
    Были добавлены такие sql запросы, как Update, Delete, Insert

--- ShopDataBase/BlogApp/Program.cs
+++ ShopDataBase/BlogApp/Program.cs
@@ -13,14 +13,35 @@ namespace BlogApp
 
         static void Main( string[] args )
         {
-            List<Statistics> statistics = ShowStatistics();
-            foreach (Statistics statistic in statistics)
+            string Operation;
+            Console.WriteLine("Введите операцию: ");
+            Operation = Convert.ToString(Console.ReadLine());
+            if (Operation == "showstats")
             {
-                Console.WriteLine("Ф.И.О.: " + statistic.Name);
-                Console.WriteLine("Количество заказов: " + statistic.NumberOfOrders);
-                Console.WriteLine("Общая стоимость покупок: " + statistic.SumOfOrders);
-                Console.WriteLine();
+                List<Statistics> statistics = ShowStatistics();
+                foreach (Statistics statistic in statistics)
+                {
+                    Console.WriteLine("Ф.И.О.: " + statistic.Name);
+                    Console.WriteLine("Количество заказов: " + statistic.NumberOfOrders);
+                    Console.WriteLine("Общая стоимость покупок: " + statistic.SumOfOrders);
+                    Console.WriteLine();
+                }
             }
+            else if (Operation == "update")
+            {
+                Update();
+            }
+            else if (Operation == "delete")
+            {
+                Delete();
+            }
+            else if (Operation == "insert")
+            {
+                int createdOrderId = InsertOrder();
+                Console.WriteLine("Created post: " + createdOrderId);
+            }
+            else
+                Console.WriteLine("Несуществующая операция: " + Operation);
         }
 
         private static List<Statistics> ShowStatistics()
@@ -60,5 +81,96 @@ namespace BlogApp
             return statistics;
         }
 
+        private static void Update()
+        {
+            int OrderId;
+            string ProductName;
+            int Price;
+            int CustomerId;
+            Console.WriteLine("Укажите Id заказа:");
+            OrderId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Укажите параметры которые вы хотете обновить (ProductName, Price, CustomerId соответственно:)");
+            ProductName = Convert.ToString(Console.ReadLine());
+            Price = Convert.ToInt32(Console.ReadLine());
+            CustomerId = Convert.ToInt32(Console.ReadLine());
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = @"
+                        UPDATE [Order]
+                        SET [ProductName] = @productname,
+                            [Price] = @price,
+                            [CustomerId] = @customerId
+                        WHERE OrderId = @orderId";
+
+                    command.Parameters.Add("@orderId", SqlDbType.BigInt).Value = OrderId;
+                    command.Parameters.Add("@productname", SqlDbType.NVarChar).Value = ProductName;
+                    command.Parameters.Add("@price", SqlDbType.Int).Value = Price;
+                    command.Parameters.Add("@customerId", SqlDbType.Int).Value = CustomerId;
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private static void Delete()
+        {          
+            string ProductName;
+            int CustomerId;
+            Console.WriteLine("Введите Id покупателя и продукт который хотите удалить из заказа соответственно");
+            CustomerId = Convert.ToInt32(Console.ReadLine());
+            ProductName = Convert.ToString(Console.ReadLine());
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = @"
+                        DELETE FROM [Order]
+                        WHERE [ProductName] = @productname
+                        AND [CustomerId] = @customerId";
+                    command.Parameters.Add("@productname", SqlDbType.NVarChar).Value = ProductName;
+                    command.Parameters.Add("@customerId", SqlDbType.Int).Value = CustomerId;
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private static int InsertOrder()
+        {
+            string ProductName;
+            int Price;
+            int CustomerId;
+            Console.WriteLine("Введите наименование продукта, цену, и Id покупателя, соответственно");
+            ProductName = Convert.ToString(Console.ReadLine());
+            Price = Convert.ToInt32(Console.ReadLine());
+            CustomerId = Convert.ToInt32(Console.ReadLine());
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (SqlCommand cmd = connection.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    INSERT INTO [Order]
+                       ([ProductName],
+                        [Price],
+                        [CustomerId]) 
+                    VALUES 
+                       (@productname,
+                        @price,
+                        @customerId)
+                    SELECT SCOPE_IDENTITY()";
+
+                    cmd.Parameters.Add("@productname", SqlDbType.NVarChar).Value = ProductName;
+                    cmd.Parameters.Add("@price", SqlDbType.Int).Value = Price;
+                    cmd.Parameters.Add("@customerId", SqlDbType.Int).Value = CustomerId;
+
+                    return Convert.ToInt32(cmd.ExecuteScalar());
+                }
+            }
+        }
     }
 }

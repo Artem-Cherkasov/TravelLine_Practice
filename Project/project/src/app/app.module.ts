37d9860a12af7a27eb1509f7f4ec2174ba877232
Artem-Cherkasov <79924549+Artem-Cherkasov@users.noreply.github.com>
2021-05-04 15:57:37 +0300

    Компоненты страницы (JS)

---
+++ Project/project/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { RouterModule } from '@angular/router';
+import { RecipelistComponent } from './recipelist/recipelist.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainpageComponent,
+    RecipelistComponent,
+    FavoritesComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      { path: '', component: MainpageComponent },
+      { path: 'main', component: MainpageComponent },
+      { path: 'list', component: RecipelistComponent },
+      { path: 'favs', component: FavoritesComponent },
+    ])
+  ],
+  exports: [ RouterModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
